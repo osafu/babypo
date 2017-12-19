329ce2e021a30bbd951e99d69cf4da9f2da17b4c
長船託也 <osa929@gmail.com>
2017-12-20 01:15:48 +0900

    Add UserId To Spots

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171203151920) do
+ActiveRecord::Schema.define(version: 20171219160509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20171203151920) do
     t.string "diaper_stand"
     t.string "baby_scales"
     t.text "memo"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
