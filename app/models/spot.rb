93b6e03735f49d1283ce72339e74bc9537644bbd
長船託也 <osa929@gmail.com>
2017-12-23 21:46:28 +0900

    Fix Validate To Spot

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,6 @@
 class Spot < ApplicationRecord
   validates :name, presence: true
-  validates :postal_code, length: { is: 7 }
+  validates :postal_code, length: { is: 7 }, numericality: { only_integer: true }
   validates :region, presence: true
   validates :address, presence: true, length: { maximum: 200 }
   validates :memo, length: { maximum: 2000 }
