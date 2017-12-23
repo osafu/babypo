974d0e65e351afcce3887c6ce259495b0267a392
長船託也 <osa929@gmail.com>
2017-12-23 16:13:33 +0900

    Fix Style To Spots

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  before_action :set_comment, only: [:edit, :destroy, :update]
+
   def create
     # spotをパラメータの値から探し出し,spotに紐づくcommentsとしてbuildします。
     @comment = current_user.comments.build(comment_params)
@@ -9,8 +12,18 @@ class CommentsController < ApplicationController
         format.html { redirect_to spot_path(@spot), notice: 'コメントを投稿しました。' }
         format.js { render :index }
       else
-        format.html { render :new }
+        flash.now[:notice] = 'コメントが未入力です。'
+        format.html { render :index, notice: 'コメントが未入力です。' }
+        format.js { render :index }
       end
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+        format.html { redirect_to spot_path(@spot), notice: 'コメントを削除しました。' }
+        format.js { render :index }
     end
   end
 
@@ -18,4 +31,9 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:spot_id, :content)
     end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
+  validates :content, presence: true, length: { maximum: 200 }
   belongs_to :user
   belongs_to :spot
 end
